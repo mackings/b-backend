de77ff629d1215881b0c259ca91ae3a98bb2b5cb
mackingsley <kingsleyudoma2018@gmail.com>
2024-06-14 13:21:35 +0100

    update

--- src/controllers/trade/activetrade.js
+++ src/controllers/trade/activetrade.js
@@ -37,3 +37,40 @@ exports.getActive = async (req, res) => {
         );
     }
 };
+
+
+
+
+
+exports.getBank = async (req, res) => {
+    try {
+        const accessToken = await getAccessToken();
+        const apiRoute = '/trade/share-linked-bank-account'; 
+        
+        // Create URLSearchParams with trade_hash
+        const params = new URLSearchParams();
+        params.append('trade_hash', req.body.trade_hash);
+
+        const response = await axios.post(
+            `${baseUrl}${apiRoute}`, 
+            params.toString(), 
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                params: req.query 
+            }
+        );
+        return res.status(200).json(successResponse('Trades data retrieved successfully', response.data));
+    } catch (error) {
+        console.error('Error making API request:', error);
+        return res.status(error.response ? error.response.status : 500).json(
+            errorResponse(
+                'Failed to fetch trades data',
+                error.response ? error.response.status : 500,
+                error
+            )
+        );
+    }
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,13 @@
 const { getUser } = require('../controllers/account/accounts');
 const { getTrades } = require('../controllers/trade/alltrades');
-const { getActive} = require('../controllers/trade/activetrade');
+const { getActive, getBank} = require('../controllers/trade/activetrade');
 
 const router = require('express').Router();
 
 router.get("/Api/getAccount", getUser);
 router.get("/Api/getTrades", getTrades);
 router.post("/Api/getActive", getActive);
+router.post("/Api/getActive", getBank);
 
 
 module.exports = router;
