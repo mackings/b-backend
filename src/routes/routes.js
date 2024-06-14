0fcfa95f9c3f5e8dd66dd227bac2cb4a95ad767c
mackingsley <kingsleyudoma2018@gmail.com>
2024-06-14 11:37:51 +0100

    update

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,10 @@
 const { getUser } = require('../controllers/account/accounts');
+const { getTrades } = require('../controllers/trade/alltrades');
 
 const router = require('express').Router();
 
 router.get("/Api/getAccount", getUser);
+router.get("/Api/getTrades", getTrades);
 
 
 module.exports = router;
