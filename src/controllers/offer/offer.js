6004a9e6e72b4612ca11bd5d0a3e0e9d48464b6e
mackingsley <kingsleyudoma2018@gmail.com>
2024-06-14 15:54:28 +0100

    update

--- src/controllers/offer/offer.js
+++ src/controllers/offer/offer.js
@@ -8,35 +8,35 @@ const baseUrl = process.env.BASE_URL;
 
 exports.getOffer = async (req, res) => {
     try {
-      
         console.log('Request body:', req.body);
 
         const accessToken = await getAccessToken();
         const apiRoute = '/offer/all'; 
         const params = new URLSearchParams();
 
-        params.append('offer_type', req.body.offer_type);
-        params.append('type', req.body.type);
-        params.append('group', req.body.group);
-        params.append('limit', req.body.limit);
-        params.append('offset', req.body.offset);
-        params.append('fiat_min', req.body.fiat_min);
-        params.append('geoname_id', req.body.geoname_id);
-        params.append('margin_max', req.body.margin_max);
-        params.append('margin_min', req.body.margin_min);
-        params.append('offer_tags', req.body.offer_tags);
-        params.append('user_types', req.body.user_types);
-        params.append('location_id', req.body.location_id);
-        params.append('user_country', req.body.user_country);
-        params.append('currency_code', req.body.currency_code);
-        params.append('payment_method', req.body.payment_method);
-        params.append('fiat_amount_max', req.body.fiat_amount_max);
-        params.append('fiat_amount_min', req.body.fiat_amount_min);
-        params.append('apply_user_restrictions', req.body.apply_user_restrictions);
+        // Parse and append parameters
+        if (req.body.offer_type) params.append('offer_type', req.body.offer_type);
+        if (req.body.type) params.append('type', req.body.type);
+        if (req.body.group) params.append('group', req.body.group);
+        if (req.body.limit) params.append('limit', parseInt(req.body.limit, 10));  // Ensure limit is an integer
+        if (req.body.offset) params.append('offset', req.body.offset);
+        if (req.body.fiat_min) params.append('fiat_min', req.body.fiat_min);
+        if (req.body.geoname_id) params.append('geoname_id', req.body.geoname_id);
+        if (req.body.margin_max) params.append('margin_max', req.body.margin_max);
+        if (req.body.margin_min) params.append('margin_min', req.body.margin_min);
+        if (req.body.offer_tags) params.append('offer_tags', req.body.offer_tags);
+        if (req.body.user_types) params.append('user_types', req.body.user_types);
+        if (req.body.location_id) params.append('location_id', req.body.location_id);
+        if (req.body.user_country) params.append('user_country', req.body.user_country);
+        if (req.body.currency_code) params.append('currency_code', req.body.currency_code);
+        if (req.body.payment_method) params.append('payment_method', req.body.payment_method);
+        if (req.body.fiat_amount_max) params.append('fiat_amount_max', req.body.fiat_amount_max);
+        if (req.body.fiat_amount_min) params.append('fiat_amount_min', req.body.fiat_amount_min);
+        if (req.body.apply_user_restrictions) params.append('apply_user_restrictions', req.body.apply_user_restrictions);
 
         const response = await axios.post(
             `${baseUrl}${apiRoute}`, 
-            params,
+            params.toString(),  // Convert URLSearchParams to string
             {
                 headers: {
                     Authorization: `Bearer ${accessToken}`,
