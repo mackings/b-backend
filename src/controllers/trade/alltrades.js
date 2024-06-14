a2a5b59356f71283453abf90228da51a6ee3dcd4
mackingsley <kingsleyudoma2018@gmail.com>
2024-06-14 11:35:00 +0100

    updates

---
+++ src/controllers/trade/alltrades.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const  getAccessToken  = require('../../utils/accesstoken');
+const { successResponse, errorResponse } = require('../../utils/responses');
+require('dotenv').config();
+const baseUrl = process.env.BASE_URL;
+
+
+
+exports.getTrades = async (req, res) => {
+    try {
+        const accessToken = await getAccessToken();
+        const apiRoute = '/trades'; // Define your API route
+        const response = await axios.get(
+            `${baseUrl}${apiRoute}`, 
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                params: req.query // If you have query parameters, they can be added here
+            }
+        );
+        return res.status(200).json(successResponse('Trades data retrieved successfully', response.data));
+    } catch (error) {
+        console.error('Error making API request:', error);
+        return res.status(error.response ? error.response.status : 500).json(
+            errorResponse(
+                'Failed to fetch trades data',
+                error.response ? error.response.status : 500,
+                error
+            )
+        );
+    }
+};
