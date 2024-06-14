ee7d04f6c8e81e3cb9a3591f6645423d809e4142
mackingsley <kingsleyudoma2018@gmail.com>
2024-06-14 14:03:00 +0100

    update

--- src/controllers/offer/offer.js
+++ src/controllers/offer/offer.js
@@ -5,14 +5,26 @@ require('dotenv').config();
 const baseUrl = process.env.BASE_URL;
 
 
+
+
+
 exports.getOffer = async (req, res) => {
     try {
+      
+        console.log('Request body:', req.body);
+
         const accessToken = await getAccessToken();
-        const apiRoute = '/offer/get'; 
-        
-        // Create URLSearchParams with trade_hash
+        const apiRoute = '/offer/list'; 
+
+        // Check if required parameters are defined
+        if (!req.body.offer_hash || !req.body.another_param) {
+            return res.status(400).json(errorResponse('offer_hash and another_param are required', 400));
+        }
+
+        // Create URLSearchParams with the parameters
         const params = new URLSearchParams();
-        params.append('offer_hash', req.body.offer_hash);
+        params.append('active', req.body.active);
+        params.append('offer_type', req.body.offer_type);
 
         const response = await axios.post(
             `${baseUrl}${apiRoute}`, 
@@ -25,12 +37,14 @@ exports.getOffer = async (req, res) => {
                 params: req.query 
             }
         );
-        return res.status(200).json(successResponse('Trades data retrieved successfully', response.data));
+
+        console.log('Response:', response.data);
+        return res.status(200).json(successResponse('Offer data retrieved successfully', response.data));
     } catch (error) {
         console.error('Error making API request:', error);
         return res.status(error.response ? error.response.status : 500).json(
             errorResponse(
-                'Failed to fetch trades data',
+                'Failed to fetch offer data',
                 error.response ? error.response.status : 500,
                 error
             )
