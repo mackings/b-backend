80919e5f2f4ccc9d5b82f6d1fe6f4787ef7f55f0
mackingsley <kingsleyudoma2018@gmail.com>
2024-06-14 12:30:15 +0100

    getactive

---
+++ src/controllers/trade/activetrade.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const  getAccessToken  = require('../../utils/accesstoken');
+const { successResponse, errorResponse } = require('../../utils/responses');
+require('dotenv').config();
+const baseUrl = process.env.BASE_URL;
+
+
+exports.getActive = async (req, res) => {
+    try {
+        const accessToken = await getAccessToken();
+        const apiRoute = '/trade/get'; 
+        const response = await axios.get(
+            `${baseUrl}${apiRoute}`, 
+            new URLSearchParams(),
+
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                params: req.query 
+            }
+        );
+        return res.status(200).json(successResponse('Trades data retrieved successfully', response.data));
+    } catch (error) {
+        console.error('Error making API request:', error);
+        return res.status(error.response ? error.response.status : 500).json(
+            errorResponse(
+                'Failed to fetch trades data',
+                error.response ? error.response.status : 500,
+                error
+            )
+        );
+    }
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 const { getUser } = require('../controllers/account/accounts');
 const { getTrades } = require('../controllers/trade/alltrades');
+const { getActive} = require('../controllers/trade/activetrade');
 
 const router = require('express').Router();
 
 router.get("/Api/getAccount", getUser);
 router.get("/Api/getTrades", getTrades);
+router.get("/Api/getActive", getActive);
 
 
 module.exports = router;
