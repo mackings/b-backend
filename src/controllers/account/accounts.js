1c5e4177458b9bfd9572fa60ea8a0a0db45c73c0
mackingsley <kingsleyudoma2018@gmail.com>
2024-06-14 10:38:39 +0100

    fixes

--- src/controllers/account/accounts.js
+++ src/controllers/account/accounts.js
@@ -1,32 +1,33 @@
 const axios = require('axios');
-const { getAccessToken } = require('../../utils/accesstoken');
+const  getAccessToken  = require('../../utils/accesstoken');
 const { successResponse, errorResponse } = require('../../utils/responses');
 require('dotenv').config();
 
 const apiUrl = process.env.API_URL;
 
+
+
 exports.getUser = async (req, res) => {
-  try {
-    const accessToken = await getAccessToken();
-    const response = await axios.post(
-      apiUrl,
-      new URLSearchParams(), 
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
-    );
-    return res.status(200).json(successResponse('User data retrieved successfully', response.data));
-  } catch (error) {
-    console.error('Error making API request:', error.response ? error.response.data : error.message);
-    return res.status(error.response ? error.response.status : 500).json(
-      errorResponse(
-        'Failed to fetch user data',
-        error.response ? error.response.status : 500,
-        error.response ? error.response.data : { message: error.message }
-      )
-    );
-  }
+    try {
+        const accessToken = await getAccessToken();
+        const response = await axios.post(
+            apiUrl,
+            new URLSearchParams(), 
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }
+        );
+        return res.status(200).json(successResponse('User data retrieved successfully', response.data));
+    } catch (error) {
+        return res.status(error.response ? error.response.status : 500).json(
+            errorResponse(
+                'Failed to fetch user data',
+                error.response ? error.response.status : 500,
+                error
+            )
+        );
+    }
 };
